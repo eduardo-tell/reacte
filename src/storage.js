aa795bd33cda0f7abd4f97fe3abd53fbb3a2b437
Eduardo Tell <eduardo-tell@hotmail.com>
2023-05-01 18:52:46 -0300

    Ativando localstorage

--- src/storage.js
+++ src/storage.js
@@ -2,11 +2,21 @@ import { configureStore } from "@reduxjs/toolkit"
 import repositoriesReducer from "./features/repositories/repositories"
 import favoritesReducer from "./features/favorites/favorites"
 
+const localStorageMiddleware = store => next => action => {
+  const result = next(action)
+  localStorage.setItem('myReduxState', JSON.stringify(store.getState()))
+  return result
+}
+
+const persistedState = localStorage.getItem('myReduxState') ? JSON.parse(localStorage.getItem('myReduxState')) : {}
+
 const store = configureStore({
   reducer: {
     repositories: repositoriesReducer,
     favorites: favoritesReducer
-  }
+  },  
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(localStorageMiddleware),
+  preloadedState: persistedState,
 })
 
 export default store
