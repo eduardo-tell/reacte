198385f253673a99a77c058cd27888669468a14d
Eduardo Tell <you@example.com>
2023-10-12 22:30:09 -0300

    Fazendo ajax dos produtos usando strapi

--- src/features/favorites/favorites.js
+++ src/features/favorites/favorites.js
@@ -8,9 +8,15 @@ const favorites = createSlice({
       if (action.payload) {
         var index = state.map(favorite => favorite.id).indexOf(action.payload.id)
 
+        action.payload.isFavorited = true
+
+        console.log(action.payload.isFavorited);
+
         if (index === -1) {
+        //   action.payload.isFavorited = 1
           state.push(action.payload)
         } else {
+        //   action.payload.isFavorited = 0
           state.splice(index, 1)
         }
       }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,22 @@ import { useFetch } from '../hooks/useFetch';
 export default function Home() {
   const dispatch = useDispatch();
   const products = useSelector(state => state.products)
-  const { data: productsStart } = useFetch("https://dummyjson.com/products?limit=8")
+  var { data: productsStart } = []
+
+
+    fetch("http://localhost:1337/api/products?sort", {
+        headers: {
+            Authorization: "57f2dae90a53857f23f8ed117e8f516b9302d09b6e4afdb9434b5a9f3c889d8e5d1f6fd06f7033ecf14279c0406c589fcec607f2452b73ba302e18d642336f5932184a82baea5a6ddc7a5e8244ab09883ca880327fe5d86255e70fb9a327a18d7d74e803ef49052645be5aab7a630593d34c990bbc06da31ce10ce01916c1a41",
+        }
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        productsStart = useFetch("https://dummyjson.com/products?limit=8")
+
+        // response.data.map(item => {
+        //     console.log(item);
+        // })
+    })
 
   // Salvando no cache os primeiros dados da API
   useEffect(() => {
